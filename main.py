2985170d152a2ab43230b82352cff6e040d3aa69
ASTRO01-max <mfulsefer@gmail.com>
2025-02-20 10:41:19 +0500

    car

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from modul import Car, Electronic, Truck, Super, Sales, read, write
+from modul import Car, Electronic, Truck, Super, Sales
+import json
+
 
 car1 = Car("Toyota", "Camry", 2022, 30000, 5)
 car2 = Electronic("Tesla", "Model S", 2023, 80000, "Elektr", 100)
@@ -11,31 +13,49 @@ print(sales.add(car1, car1.price))
 print(sales.add(car2, car2.price))
 print(sales.add(car3, car3.price))
 print(sales.add(car4, car4.price))
-print(sales.remove(car3))
+
+print(sales.remove(car3))  
+
 print("________________________________________")
 print(sales.total_price())
 sales.show_sold_cars()
+
+path = "orders.json"
+
+def read():
+    try:
+        with open(path, "r") as file:
+            data = json.load(file)
+        return data
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def write(data):
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
 
 menu = ("1 - saqlash\n"
         "2 - o'qish\n"
         "3 - chiqib ketish\n")
 
 while True:
-    print("")
-    print(menu)
-    choice = int(input("enter num: "))
+    print("\n", menu)
+    choice = input("Enter num: ").strip()
 
-    if choice == 1:
+    if choice == "1":
         users = read()
-        users += [car1, car2, car3, car4]
+        users += [car.to_dict() for car in [car1, car2, car4]]  
         write(users)
-        print("data saved.")
+        print("Data saved.")
 
-    elif choice == 2:
+    elif choice == "2":
         users = read()
-        for user in users:
-            print(user)
+        if not users:
+            print("Ma'lumotlar mavjud emas.")
+        else:
+            for user in users:
+                print(user)
     
-    elif choice == 3:
+    elif choice == "3":
+        print("Dastur yakunlandi.")
         break
-    
